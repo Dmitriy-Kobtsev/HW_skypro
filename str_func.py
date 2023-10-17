9826f9e9a49ce0bd9916c48469bda1b2c58f5f3f
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2023-10-18 00:20:29 +0300

    add func get_str

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def get_up(str: str):
+    """Возвращает заглавные буквы"""
     return str.upper()
+
+
+def get_str(str: str):
+    """Делает заглавными первые буквы каждого слова"""
+    return str.title()
